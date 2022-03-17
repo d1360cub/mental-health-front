6efd90468aeb3fc57d8840d357655048e544ac60
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-03-17 13:20:02 -0500

    creates new user with register form

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom"
 import "./Register.css";
+import newUser from "../../services/user";
 
 function Register() {
+  const [form, setForm] = useState({});
+  const handleChange = (event) => {
+    const {value,name} = event.target;
+    setForm({
+      ...form,
+      [name]: value
+    });
+    console.log(form)
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    newUser(form);
+    setForm({});
+  }
   return (
     <div className="register">
       <div className="title">Mental Health</div>
@@ -17,32 +32,32 @@ function Register() {
           <Link to="/">¿Paciente?</Link>
         </div>
       </div>
-      <form action="" method="post">
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <label for="name">Nombre</label>
-          <input type="text" id="name" name="name" />
+          <input type="text" id="name" name="name" onChange={handleChange}/>
         </fieldset>
         <fieldset>
           <label for="last-name">Apellido</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" onChange={handleChange}/>
         </fieldset>
         <fieldset>
           <label for="cellphone">Celular</label>
-          <input type="tel" id="cellphone" name="cellphone" />
+          <input type="tel" id="cellphone" name="cellphone" onChange={handleChange}/>
         </fieldset>
         <fieldset>
           <label for="email">Email</label>
-          <input type="email" id="email" name="email" />
+          <input type="email" id="email" name="email" onChange={handleChange}/>
         </fieldset>
         <fieldset>
           <label for="password">Contraseña</label>
-          <input type="password" id="password" name="password" />
+          <input type="password" id="password" name="password" onChange={handleChange}/>
         </fieldset>
-      </form>
       <div className="session">
         ¿Ya eres un miembro? <a href="/login.html">Inicia sesión</a>
       </div>
-      <button className="register">Regístrate</button>
+      <button type="submit">Regístrate</button>
+      </form>
     </div>
   );
 }
