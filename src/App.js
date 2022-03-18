25867a61d1d825178398207847aa5374b4f55b8c
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2022-03-18 13:59:01 -0500

    feat: rutas protegidas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Register from './Pages/Register/Register';
 import ViewerDoctor from "./Pages/ViewerDoctor";
 import ViewerPatient from "./Pages/ViewerPatient";
 import Login from "./Pages/Login/Login";
+import ProtectedRoutes from "./Routes/ProtectedRoutes";
+import Page404 from "./Pages/Page404/Page404";
 
 function App() {
   return (
@@ -15,9 +17,13 @@ function App() {
       <Routes>
         <Route path="/" exact element={ <HomePage /> } />
         <Route path="/register" element={ <Register />} />
-        <Route path="/viewerDoctor" element={ <ViewerDoctor />} />
-        <Route path="/viewerPatient" element={ <ViewerPatient />} />
-        <Route path="/login" element={ <Login />} /> 
+        <Route path="/login" element={ <Login />} />
+        <Route element={ <ProtectedRoutes/> } >
+          <Route path="/viewerDoctor" element={ <ViewerDoctor />} />
+          <Route path="/viewerPatient" element={ <ViewerPatient />} />
+        </Route>
+
+        <Route path="*" element={ <Page404 /> } />
       </Routes>
     </BrowserRouter>
   );
