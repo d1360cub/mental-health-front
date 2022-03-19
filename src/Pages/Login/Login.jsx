d62cc8ba9a03f2ac3dd706c92cd9dd76d2c19394
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-03-18 22:38:57 -0500

    adds navigation to login and register

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -5,38 +5,35 @@ import './Login.css';
 
 function Login() {
   return (
-    <div>
-      <div className="landing">
-        <section className="register" id="register">
-          <div className="subtitle">Iniciar sesión</div>
-          <form action="login" onSubmit="">
-            <fieldset>
-              <label htmlFor="username">
-                Email *
-                <input type="text" id="username" name="username" />
-              </label>
-            </fieldset>
-            <fieldset>
-              <label htmlFor="password">
-                Contraseña *
-                <input type="password" id="password" name="password" />
-              </label>
-            </fieldset>
-          </form>
-          <div className="click">
-            <Link to="/" className="password">¿Olvidaste tu contraseña?</Link>
-            <Link to="/ViewerPatient" className="question">
-              <button className="btn-appointment" type="submit">Iniciar sesión</button>
-            </Link>
-          </div>
-          <div className="first-time">
-            ¿Es tu primera vez?
-            {' '}
-            <Link to="/register" className="question">Regístrate</Link>
-          </div>
-        </section>
-        <img src={LoginImage} alt="login" className="image" />
-      </div>
+    <div className="landing">
+      <section className="register" id="register">
+        <div className="subtitle">Iniciar sesión</div>
+        <form action="login" onSubmit="">
+          <fieldset>
+            <label htmlFor="username">
+              Email *
+              <input type="text" id="username" name="username" size="25" />
+            </label>
+          </fieldset>
+          <fieldset>
+            <label htmlFor="password">
+              Contraseña *
+              <input type="password" id="password" name="password" size="20" />
+            </label>
+          </fieldset>
+        </form>
+        <div className="click">
+          <Link to="/" className="password">¿Olvidaste tu contraseña?</Link>
+          <Link to="/ViewerPatient" className="question">
+            <button className="btn-appointment" type="submit">Iniciar sesión</button>
+          </Link>
+        </div>
+        <div className="first-time">
+          ¿Es tu primera vez?
+          <Link to="/register" className="question">Regístrate</Link>
+        </div>
+      </section>
+      <img src={LoginImage} alt="login" className="image" />
     </div>
   );
 }
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -19,53 +19,82 @@ function Register() {
   };
   return (
     <div className="register">
-      <div className="landing">
-        <div className="subtitle">Registro</div>
+      <div className="landingReg">
+        <div className="subtitleReg">Registro</div>
         <div className="question">
-          <div className="question__doctor">
-            {' '}
-            <Link to="/">¿Quieres prestar tus servicios con nosotros?</Link>
-          </div>
+          <Link to="/">¿Quieres prestar tus servicios con nosotros?</Link>
         </div>
         <form onSubmit={handleSubmit}>
           <fieldset>
             <label htmlFor="name">
               Nombre
-              <input type="text" id="name" name="name" onChange={handleChange}/>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                onChange={handleChange}
+                size="20"
+              />
             </label>
           </fieldset>
           <fieldset>
             <label htmlFor="last-name">
               Apellido
-              <input type="text" id="last-name" name="last-name" onChange={handleChange} />
+              <input
+                type="text"
+                id="last-name"
+                name="last-name"
+                onChange={handleChange}
+                size="20"
+              />
             </label>
           </fieldset>
           <fieldset>
             <label htmlFor="cellphone">
               Celular
-              <input type="tel" id="cellphone" name="cellphone" onChange={handleChange} />
+              <input
+                type="tel"
+                id="cellphone"
+                name="cellphone"
+                onChange={handleChange}
+                size="20.5"
+              />
             </label>
           </fieldset>
           <fieldset>
             <label htmlFor="email">
               Email
-              <input type="email" id="email" name="email" onChange={handleChange} />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                onChange={handleChange}
+                size="22"
+              />
             </label>
           </fieldset>
           <fieldset>
             <label htmlFor="password">
               Contraseña
-              <input type="password" id="password" name="password" onChange={handleChange} />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                onChange={handleChange}
+                size="17"
+              />
             </label>
           </fieldset>
-          <div className="session">
-            ¿Ya eres un miembro?
-            <Link to="/login">
-              Inicia sesión
-            </Link>
-          </div>
-          <button type="submit" className="btn-appointment">Regístrate</button>
         </form>
+        <div className="session">
+          ¿Ya eres un miembro?
+          <Link to="/login">Inicia sesión</Link>
+        </div>
+        <Link to="/ViewerDoctor">
+          <button type="submit" className="btn-appointment">
+            Regístrate
+          </button>
+        </Link>
       </div>
     </div>
   );
