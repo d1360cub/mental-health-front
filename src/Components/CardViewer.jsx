1b6b3cd8b6898995727530dd0ce6df5059dd720b
Alexander suarez rodriguez <87741454+AlexSuarezRodriguez@users.noreply.github.com>
2022-03-19 22:28:10 -0500

    Feature/eslint alex (#33)
    * cambio en seccion de enrutameinto
    
    * add db.json
    
    * cambios con eslint
    
    * correction files

--- src/Components/CardViewer.jsx
+++ src/Components/CardViewer.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/prop-types */
 import Enlace from './Enlace';
 import './CardViewer.css';
 
@@ -12,30 +14,17 @@ function CardViewer({ information, links }) {
       <div className="home-content__card--perfil">
         <h3>
           {information.name}
-          {' '}
           {information.lastName}
         </h3>
         <p>
-          <span>
-            {information.services}
-            ,
-            {' '}
-          </span>
-          <span>
-            {information.phone}
-            ,
-            {' '}
-          </span>
-          <span>
-            {information.mail}
-            ,
-            {' '}
-          </span>
+          <span>{information.services}, </span>
+          <span>{information.phone}, </span>
+          <span>{information.mail}, </span>
         </p>
         <div className="home_content__enlaces">
-          {
-          links.map((element) => <Enlace path={element.path} url={element.url} key={element.id} />)
-        }
+          {links.map((element) => (
+            <Enlace path={element.path} url={element.url} key={element.id} />
+          ))}
         </div>
       </div>
     </div>
