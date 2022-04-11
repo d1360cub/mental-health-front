ad02d3e87f104d9404bc81766ec4df6bf18ba6fc
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-04-11 17:40:51 -0500

    add services to redux's store

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './Services.css';
-import getAllServices from '../../services/serviceServices';
+// import getAllServices from '../../services/serviceServices';
 import ServiceCard from '../../Components/ServiceCard/ServiceCard';
+import { showAllServices } from '../../store/actions';
 
 export default function Services() {
-  const [services, setServices] = useState([]);
-
-  const getServices = async () => {
+  // const [services, setServices] = useState([]);
+  const dataServices = useSelector((state) => state.services);
+  const dispatch = useDispatch();
+  /* const getServices = async () => {
     const data = await getAllServices();
     setServices(data);
-  };
+  }; */
 
   useEffect(() => {
-    getServices();
+    dispatch(showAllServices());
   }, []);
-
+  console.log(dataServices);
   return (
     <section className="services" id="services">
       <h1 className="section-heading">
         <span>Servicios</span>
       </h1>
       <div className="services__box-container">
-        {services.map((servicio, index) => (
+        {dataServices.map((servicio) => (
           <ServiceCard
-            // eslint-disable-next-line react/no-array-index-key
-            key={index}
+            // eslint-disable-next-line no-underscore-dangle
+            key={servicio._id}
             // eslint-disable-next-line no-underscore-dangle
             id={servicio._id}
             service={servicio.title}
