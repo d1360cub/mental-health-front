5b115a013e2d6e19b2eef063baa77e4148247b62
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2022-04-14 18:42:43 -0500

    feat: data fake for sliders

---
+++ src/dataFake.js
@@ -0,0 +1,28 @@
+export const dataSliders = [
+  {
+    idSlide: 1,
+    imgSlide: 'https://i.ibb.co/zNVCB3c/home-mind-img.png',
+    altImg: 'alt slider 1',
+    titleSlide: 'El Cerebro Emocional',
+    descriptionSlide: 'Diversos autores llaman así al sistema límbico. Este es el encargado de regular las emociones, pero no es su única función, tambien tiene un papel importante en el aprendizaje, memoria y en respuestas fisiológicas ante ciertos estímulos.',
+    colorSlide: 'f5f5f5',
+  },
+  {
+    idSlide: 2,
+    imgSlide: 'https://i.ibb.co/XZHZfqQ/about-us-img.png',
+    altImg: 'alt slider 2',
+    titleSlide: 'Sin contratos',
+    descriptionSlide: 'Paga sólo cuando lo necesites y hazlo desde donde quieras.Deja de pagar costosas consultas ambulatorias. Nuestro concepto de &apos;consulta por uso&apos; te da la garantía de acceder a un psicólogo de forma fácil, rápida y económica.',
+    colorSlide: 'fcf1ed',
+  },
+  {
+    idSlide: 3,
+    imgSlide: 'https://i.ibb.co/L52MdQ3/psycoterapy-img.png',
+    altImg: 'alt slider 3',
+    titleSlide: 'Psicoterapia?',
+    descriptionSlide: 'La psicoterapia ayuda a las personas a encontrar sus propios recursos y respuestas. Nuestra funcion es la de acompaniar y facilitar el proceso de cambio y transformación de consultante.',
+    colorSlide: 'f5f5f5',
+  },
+];
+
+export const dataCategories = [];
