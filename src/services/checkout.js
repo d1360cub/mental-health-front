9a95a968acd4a734cd5dd631d686b33ff4058fe1
Diego Cubides <95053070+d1360cub@users.noreply.github.com>
2022-04-22 23:38:38 -0500

    Stripe (#78)
    * adds stripe payments
    
    * adds checkout
    
    * adds payment and confirm appointment feature
    
    Co-authored-by: ojosdegatonegro <diegofernandocubides@gmail.com>

---
+++ src/services/checkout.js
@@ -0,0 +1,21 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
+const checkout = async (error, paymentMethod, token) => {
+  try {
+    const payload = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `bearer ${token}`,
+      },
+      body: JSON.stringify({ paymentMethod, amount: 5_000 }),
+    };
+    const response = await fetch(`${API_URL}/api/checkout`, payload);
+    const body = await response.json();
+    return body;
+  } catch {
+    throw new Error(error);
+  }
+};
+
+export default checkout;
