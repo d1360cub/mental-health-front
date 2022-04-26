735716cec303f0aec62a545e5ad39d374efa5770
wendy-medina <endyleyms@gmail.com>
2022-04-26 10:41:06 -0500

    styles developers modified. line space cardviewr

--- src/Components/Developers/Developers.jsx
+++ src/Components/Developers/Developers.jsx
@@ -6,14 +6,13 @@ function Developers() {
   return (
     <section>
       <h1 className="section-heading"><span>Desarrolladores</span></h1>
-      <section className="developers" id="developers">
+      <div className="developers">
         <div className="box">
           <img className="fotoperfil" src="https://avatars.githubusercontent.com/u/87741454?v=4" alt="" />
           <h3>
             Alexander Suarez
           </h3>
           <h4>description</h4>
-          <p />
           <a className="linkredes" href="https://github.com/AlexSuarezRodriguez">GitHub</a>
           <a className="linkredes" href="https://www.linkedin.com/in/alexander-suarez-rodriguez/">Linkedin</a>
         </div>
@@ -23,7 +22,6 @@ function Developers() {
             Diego Cubides
           </h3>
           <h4>description</h4>
-          <p />
           <a className="linkredes" href="https://github.com/d1360cub">GitHub</a>
           <a className="linkredes" href="https://www.linkedin.com/in/diegocubides/">Linkedin</a>
         </div>
@@ -33,7 +31,6 @@ function Developers() {
             Wendy Medina
           </h3>
           <h4>description</h4>
-          <p />
           <a className="linkredes" href="https://github.com/endyleyms" src=" ">GitHub</a>
           <a className="linkredes" href="https://www.linkedin.com/in/wendy-medina-9205/">Linkedin</a>
         </div>
@@ -43,13 +40,10 @@ function Developers() {
             Pool Hijuela
           </h3>
           <h4>description</h4>
-          <p />
           <a className="linkredes" href="https://github.com/popehiflo">GitHub</a>
           <a className="linkredes" href="https://www.linkedin.com/in/popehiflo/">Linkedin</a>
         </div>
-
-      </section>
-
+      </div>
     </section>
 
   );
