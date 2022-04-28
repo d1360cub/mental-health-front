d6ad2ac273c071951f2f28a62a6d49f833bf80dd
wendy-medina <endyleyms@gmail.com>
2022-04-28 10:42:45 -0500

    persisted Store

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,33 @@ import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (!serializedState) return undefined;
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+    console.log(err);
+  }
+};
+const persistedStore = loadState();
+
 const store = createStore(
   reducers,
+  persistedStore,
   composeWithDevTools(applyMiddleware(thunk)),
 );
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
