799c7611569a9e23a8411bc69ad8f6fb8cc847c0
Wendy Medina <78057584+endyleyms@users.noreply.github.com>
2022-04-28 11:05:28 -0500

    Feature/wendy (#92)
    * css profile responsive fixed
    
    * css profile responsive fixed part 12241
    
    * css heding home fixed
    
    * css home page heading abou, service, doctor position and styles fixed
    
    * new line css files
    
    * new line css files
    
    * fixed img developers
    
    * css style fixed upload image
    
    * register patient cypress test
    
    * patient login cypress test
    
    * gitignore modified
    
    * persisted Store
    
    * persisted Store, modified try catch
    
    * fixed error

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,37 @@
+/* eslint-disable consistent-return */
 import { createStore, applyMiddleware } from 'redux';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (!serializedState) return undefined;
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+    return err;
+  }
+};
+const persistedStore = loadState();
+
 const store = createStore(
   reducers,
+  persistedStore,
   composeWithDevTools(applyMiddleware(thunk)),
 );
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
