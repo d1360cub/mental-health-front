f7708eba040915bf82810c0b5ea62be481c54d5a
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-05-03 15:04:53 -0500

    not coment

--- src/Pages/viewerPatient/ViewerPatient.jsx
+++ src/Pages/viewerPatient/ViewerPatient.jsx
@@ -6,21 +6,17 @@ import { useSelector, useDispatch } from 'react-redux';
 import CardViewer from '../../Components/CardViewer';
 import Checklist from '../../Components/Checklist/Checklist';
 import Welcome from '../../Components/Welcome';
-// import { getAppointmentsByPatientId } from '../../services/appointments';
 import { showAppointmentPatient } from '../../store/actions';
 import Calendar from '../../Components/Calendar';
 import '../HomeViewer.css';
 
 function ViewerPatient() {
   const dispatch = useDispatch();
-  // const [appointment, setAppointment] = useState([]);
   const appointment = useSelector((state) => state.appointmentsPatient);
   const patient = useSelector((state) => state.user);
   const { user, token } = patient;
 
   useEffect(async () => {
-    // const data = await getAppointmentsByPatientId(user._id);
-    // setAppointment(data);
     dispatch(showAppointmentPatient(user._id));
   }, [token]);
 
