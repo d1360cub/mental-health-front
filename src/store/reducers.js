cb650e0e090448970e5083f52c3ff2cea1f081ff
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-05-04 01:04:34 -0500

    delete appointment when from viewer doctor re-order

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-underscore-dangle */
 import {
-  RESET,
-  GET_SERVICES,
   ADD_USER,
   GET_USERS,
   LOGIN_USER,
-  GET_USER,
-  UPDATE_LIST_USERS,
+  GET_SERVICES,
   GET_APPOINTMENTS,
+  GET_USER,
   RESERVE_APPOINTMENT,
   GET_HISTORY_PATIENT,
+  RESET,
   GET_TASKS_PATIENT,
+  UPDATE_LIST_USERS,
   APPOINTMENTS_PATIENT,
   DELETE_APPOINTMENT,
 } from './types';
@@ -18,12 +18,13 @@ import {
 const initialState = {
   users: [],
   user: {},
+  services: [],
+  appointments: [],
   patient: [],
   preAppointment: {},
-  listAppointments: [],
-  services: [],
   cHistory: [],
   tasks: [],
+  listAppointments: [],
 };
 
 // eslint-disable-next-line default-param-last
@@ -57,7 +58,7 @@ function reducers(state = initialState, action) {
     case GET_APPOINTMENTS:
       return {
         ...state,
-        listAppointments: action.payload,
+        appointments: action.payload,
       };
     case GET_USER:
       return {
